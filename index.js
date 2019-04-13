3c8cfbed94aa549c85d717967356ba9478efa28d
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2019-04-13 07:39:50 +0100

    assertion

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let ins = (x) => insp(x, {depth:null});
 let exec = require('child_process').exec;
 let fs = require('fs');
 
-let runTest = ({executable, programName, assertion, done}) => {
+let runTest = ({executable, exampleInput, assertion, done}) => {
   let result = '';
-  let expected = fs.readFileSync(`test/examples/${programName}.expected`, 'utf8');
+  let expected = fs.readFileSync(`test/examples/${exampleInput}.expected`, 'utf8');
   
-  let program = `${executable} test/examples/${programName}`;
+  let program = `${executable} test/examples/${exampleInput}`;
  
   let child = exec(program);
 
@@ -16,7 +16,7 @@ let runTest = ({executable, programName, assertion, done}) => {
   });
 
   child.on('close', function() {
-    assertion(); // result.trim().should.eql(expected.trim());
+    assertion(result, expected); // result.trim().should.eql(expected.trim());
     done();
   });
 };
