626855e96d5570c0320ee16a37f4dbd93d9cf9ae
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2019-04-13 08:29:59 +0100

    try catch

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ let exec = require('child_process').exec;
 let fs = require('fs');
 
 let runTest = ({executable, exampleInput, assertion, done}) => {
-  let result = '';
-  let expected = fs.readFileSync(`test/examples/${exampleInput}.expected`, 'utf8');
-  
-  let program = `${executable} test/examples/${exampleInput}`;
- 
-  let child = exec(program);
+  try {
+    let result = '';
+    let expected = fs.readFileSync(`test/examples/${exampleInput}.expected`, 'utf8');
+    
+    let program = `${executable} test/examples/${exampleInput}`;
+   
+    let child = exec(program);
 
-  child.stdout.on('data', function(data) {
-    result += data;
-  });
+    child.stdout.on('data', function(data) {
+      result += data;
+    });
 
-  child.on('close', function() {
-    assertion(result, expected); // result.trim().should.eql(expected.trim());
+    child.on('close', function() {
+      assertion(result, expected); // result.trim().should.eql(expected.trim());
+      done();
+    });
+  } catch (err) {
     done();
-  });
+    throw Error(`There were problems either opening file 'test/examples/${exampleInput}.expected' or executing '`${executable} test/examples/${exampleInput}'\n`+err);
+  }
 };
 
 module.exports = runTest;
